d52df1d2f1acc36da26f01b19981e7cf60275565
adeshingale3 <adeshingale600@gmail.com>
2025-08-02 22:14:43 +0530

    useCursor adde

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from './hooks/SpeechSynthesis';
 export * from './hooks/Hover';
 export * from './hooks/Fetch';
 export * from './hooks/SessionTimeout';
+export * from './hooks/Cursor';
 
 export { default as FollowCursor } from './components/cursor/FollowCursor/FollowCursor';
