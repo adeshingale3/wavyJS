51981a874722f9ebea9e70900fe853626423c9c7
adeshingale3 <adeshingale600@gmail.com>
2025-08-19 13:01:57 +0530

    Java Card Icon added

--- src/components/Card-Icons/index.ts
+++ src/components/Card-Icons/index.ts
@@ -1,3 +1,5 @@
 export { default as ReactCardIcon } from './ReactCardIcon';
 export { default as JavascriptCardIcon } from './JavascriptCardIcon';
+export { default as JavaCardIcon } from './JavaCardIcon';
+
 
