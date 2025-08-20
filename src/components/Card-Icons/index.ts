dc2dad7ffd8c69e19bf00acd6ff4a3b9dc4ee238
adeshingale3 <adeshingale600@gmail.com>
2025-08-20 11:44:59 +0530

    css card added

--- src/components/Card-Icons/index.ts
+++ src/components/Card-Icons/index.ts
@@ -3,5 +3,6 @@ export { default as JavascriptCardIcon } from './JavascriptCardIcon';
 export { default as JavaCardIcon } from './JavaCardIcon';
 export {default as TailwindCardIcon} from './TailwindCardIcon';
 export {default as HtmlCardIcon} from './HtmlCardIcon' 
+export {default as CssCardIcon} from './CSSCardIcon'
 
 
